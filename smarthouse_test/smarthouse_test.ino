117d2de50423203e56c170c948be56d5fcbb9a41
Jannii <Johancn83@gmail.com>
2016-11-10 11:40:43 +0100

    multipol changes

--- smarthouse_test/smarthouse_test.ino
+++ smarthouse_test/smarthouse_test.ino
@@ -20,6 +20,7 @@ int powerOutage = 7;
 int alarmSiren = 12;
 int fan = 10;
 boolean burglerAlarmOn = false;
+boolean burglerCount = false;
 
 int multiplexB5 = 13;
 int multiplexB4 = 12;
@@ -46,6 +47,10 @@ void setup() {
   //Communication
   Serial.begin(9600);
   Xbee.begin(9600);
+  // variabler
+  fireCount = false;
+  burglerCount = false; 
+  burglerAlarmOn = true;
 
 }
 
@@ -54,7 +59,7 @@ void loop() {
   //stoveStatus();
   readXbee();
   fireAlarm();
-  burglerAlarm()
+  burglerAlarm();
   
 }
 void readXbee() {
@@ -70,49 +75,50 @@ void readXbee() {
   }
 }
 void fireAlarm() {
-  if (digitalRead(fire) == HIGH) {
-
-    if (fireCount == false) {
+  
+  delay(25);
+  if (digitalRead(fire) == HIGH && fireCount == false) {
       Serial.print("150001");
+      multiplex(HIGH,LOW,LOW,LOW);
       fireCount  = true;
-    }
-  } else {
-    fireCount = false;
+  } 
+ 
+  else if(digitalRead(fire) == LOW && fireCount == true){
+      Serial.print("150000");
+      multiplex(LOW,LOW,LOW,LOW);
+      fireCount  = false;
   }
 }
 void burglerAlarm(){
-  if(burglerAlarmOn == true){
-    if(digitalRead(Alarm) == LOW){
+  if(burglerAlarmOn == true && digitalRead(alarm) == LOW && burglerCount == false){
       Serial.print("160001");
-      
-    }
-     
+      burglerCount = true;
+      multiplex(LOW,LOW,HIGH,HIGH);
+  } else if(burglerAlarmOn == true && digitalRead(alarm) == HIGH && burglerCount == true){
+    Serial.print("160000");
+    multiplex(HIGH,LOW,HIGH,HIGH);
+    burglerCount = false;
   }
+
 }
 void indoorLightsOnOff() {
   if (inbytes[5] == '1') {
-
-    digitalWrite(multiplexB4, LOW);
-    digitalWrite(multiplexB5, LOW);
-    digitalWrite(multiplexB3, HIGH);
-    digitalWrite(multiplexB0, LOW);
-
+    multiplex(LOW,LOW,HIGH,LOW);
   }
   //put lights off
   else if (inbytes[5] == '0') {
-
-    digitalWrite(multiplexB4, HIGH);
-    digitalWrite(multiplexB5, LOW);
-    digitalWrite(multiplexB3, HIGH);
-    digitalWrite(multiplexB0, LOW);
-
-
+    multiplex(HIGH,LOW,HIGH,LOW);
   } else {
-    Serial.println("du vill tända/släcka lamporna inomhus men måste välja true or false");
+    Serial.print("du vill tända/släcka lamporna inomhus men måste välja true or false");
 
   }
 }
-
+void multiplex(int b4,int b5,int b3,int b0){
+  digitalWrite(multiplexB4, b4);
+    digitalWrite(multiplexB5, b5);
+    digitalWrite(multiplexB3, b3);
+    digitalWrite(multiplexB0, b0);
+}
 void checkFirst5Byte() {
   if (inbytes[0] == '1' && inbytes[1] == '1' && inbytes[2] == '0' && inbytes[3] == '0' && inbytes[4] == '0') {
     // read Attic temp
@@ -128,9 +134,11 @@ void checkFirst5Byte() {
   } else if (inbytes[0] == '1' && inbytes[1] == '4' && inbytes[2] == '0' && inbytes[3] == '0' && inbytes[4] == '0') {
     //read power consumption
   } else if (inbytes[0] == '1' && inbytes[1] == '5' && inbytes[2] == '0' && inbytes[3] == '0' && inbytes[4] == '0') {
-    //read fire alarm
+    //read fire Alarm
+    readFireAlarm();
   } else if (inbytes[0] == '1' && inbytes[1] == '6' && inbytes[2] == '0' && inbytes[3] == '0' && inbytes[4] == '0') {
     //read burgler alarm
+    readBurglerAlarm();
   } else if (inbytes[0] == '1' && inbytes[1] == '7' && inbytes[2] == '0' && inbytes[3] == '0' && inbytes[4] == '0') {
     //read water leakage
   } else if (inbytes[0] == '1' && inbytes[1] == '8' && inbytes[2] == '0' && inbytes[3] == '0' && inbytes[4] == '0') {
@@ -153,19 +161,17 @@ void checkFirst5Byte() {
     indoorLightsOnOff();
   } else if (inbytes[0] == '2' && inbytes[1] == '7' && inbytes[2] == '0' && inbytes[3] == '0' && inbytes[4] == '0') {
     // set outdoorlight on off
-    //outdoorLightsOnOff();
+   outdoorLightsOnOff();
   } else {
     for (int i = 0; i < 6; i++) {
       Serial.print(inbytes[i]);
     }
-    Serial.println("detta skickade du ");
+    Serial.print("detta skickade du ");
 
   }
 }
 
 void stoveStatus() {
-  delay(1000);
-  Serial.println(digitalRead(stove));
   if (digitalRead(stove) == HIGH) {
     Serial.print("180001");
 
@@ -173,7 +179,32 @@ void stoveStatus() {
     Serial.print("180000");
   }
 }
-void outdoorlight() {
+void outdoorLightsOnOff() {
+if (inbytes[5] == '1') {
+    multiplex(LOW,HIGH,HIGH,HIGH);
+  }
+  //put lights off
+  else if (inbytes[5] == '0') {
+    multiplex(HIGH,HIGH,HIGH,HIGH);
+  } else {
+    Serial.print("du vill tända/släcka lamporna inomhus men måste välja true or false");
 
+  }
+}
+void readFireAlarm(){
+  if (digitalRead(fire) == HIGH) {
+    Serial.print("150001");
+
+  } else {
+    Serial.print("150000");
+  }
+}
+void readBurglerAlarm(){
+  if (digitalRead(fire) == HIGH) {
+    Serial.print("160001");
+
+  } else {
+    Serial.print("160000");
+  }
 }
 
